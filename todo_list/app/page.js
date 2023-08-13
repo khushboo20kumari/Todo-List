52f9ee1bc893019b99f35b789f11ee57c7fb84ff
khushboo kumari <khushboo20@navgurukul.org>
2023-08-13 15:31:02 +0530

    created app of todo list

---
+++ todo_list/app/page.js
@@ -0,0 +1,13 @@
+import Todo_list from "./todo_list";
+function page(){
+
+   return(
+   
+       <div>
+       
+       <Todo_list/>
+          
+       </div>
+   )
+}
+export default page;
---
+++ todo_list/app/todo_list.js
@@ -0,0 +1,12 @@
+function Todo_list(){
+
+   return(
+   
+      <div className="card">
+          hello todo list
+      </div>
+      
+   )
+}
+export default Todo_list;
+
